20a9033fda2d47d54ae6612866222fa302e4542a
Nicolas Tetsuo Kimura <126641748+BoBatata@users.noreply.github.com>
2023-05-15 21:01:42 -0300

    Player Movement, Asset
    Player Movement , Assets

---
+++ FirstGameProject/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Transform playerTransform;
+    private Rigidbody2D rigidbody;
+    private bool isOnFloor;
+
+    [Header("Player movement")]
+    [SerializeField] private float velocity = 10;
+    [SerializeField] private float jumpForce = 700;
+
+
+    private void Awake()
+    {
+
+        playerTransform = GetComponent<Transform>();
+        rigidbody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        MovePlayer();
+
+    }
+
+    private void MovePlayer()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal") * velocity * Time.deltaTime;
+
+        playerTransform.Translate(new Vector3(moveX, 0));
+
+        if (Input.GetButtonDown("Jump") && isOnFloor)
+        {
+            isOnFloor = false;
+            rigidbody.AddForce(Vector2.up * jumpForce);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOnFloor = true;
+
+        if (collision.gameObject.tag == "Enemy")
+        {
+            StartCoroutine(DestroyPlayer());
+        }
+    }
+
+    IEnumerator DestroyPlayer()
+    {
+        Color c = GetComponent<Renderer>().material.color;
+        for (float alpha = 1f; alpha >= 0; alpha -= 0.1f)
+        {
+            c.a = alpha;
+            GetComponent<Renderer>().material.color = c;
+            yield return new WaitForSeconds(.1f);
+        }
+        Destroy(gameObject);
+    }
+}
